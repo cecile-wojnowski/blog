8a8fa3acda951010537896ba91fab5cfeb1e9f2b
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-06-24 12:04:54 +0200

    Ebauches conditions permettant l'affichage des catégories

--- articles.php
+++ articles.php
@@ -18,41 +18,57 @@
           [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
     $count = (int)$bdd->query('SELECT COUNT(id) FROM articles LIMIT 5')->fetch(PDO::FETCH_NUM)[0];
-    $offset = (int)((!isset($_GET['start'])) ? 0 : $_GET["start"]);
+    $offset = (int)((!isset($_GET["start"])) ? 0 : $_GET["start"]);
+
+    # Condition empêchant que l'offset soit négatif
     if ($offset < 0){
       $offset = 0;
-    }
-
-    $categorie =
+    };
 
     // On récupère les 5 derniers articles
-    $req = $bdd->query("SELECT id_categorie, categories.id, articles.id, article, date, titre
-                FROM categories, articles WHERE id_categorie = categories.id
+    $req = $bdd->query("SELECT articles.id_categorie, categories.id, articles.id, article, date, titre
+                FROM categories, articles WHERE articles.id_categorie = categories.id
                 ORDER BY date DESC LIMIT 5 OFFSET $offset");
 
-    //début de la boucle pour afficher les derniers articles
-    while ($donnees = $req->fetch()){
+
+    # Affichage des catégories
+
+    #$categorie = (!isset($_GET['categorie'])) ? 0 : $_GET['categorie'];
     ?>
-    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-      <div class="card">
-        <h2>  <?php echo htmlspecialchars($donnees['titre']); ?> <h2>
-          <p>  <?php echo htmlspecialchars($donnees['article']); ?> </p>
-          <h5>le <?php echo $donnees['date']; ?></h5>
 
-        <em><a href="article.php?article=<?php echo $donnees['id']; ?>"  >voir l'article</a></em>
-      </div>
-    </div>
+    <a href="articles.php?categorie=1&start=<?php echo $offset ?>" class="categorie"> Philosophie </a>
+    <a href="articles.php?categorie=2&start=<?php echo $offset ?>" class="categorie"> Littérature </a>
 
-    <?php };
+    <?php
+    # Si get catégorie n'est pas utilisé...
+    if (!isset($_GET['categorie'])){
+      //Afficher les 5 derniers articles
+      while ($donnees = $req->fetch()){
+      ?>
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+          <div class="card">
 
-    # Liens "Page précédente" et "Page suivante" ?>
+            <h2>  <?php echo htmlspecialchars($donnees['titre']); ?> </h2>
+              <p>  <?php echo htmlspecialchars($donnees['article']); ?> </p>
+              <h5>le <?php echo $donnees['date']; ?></h5>
+
+            <em><a href="article.php?article=<?php echo $donnees['id']; ?>"  >voir l'article</a></em>
+          </div>
+        </div>
+      <?php
+      }
+    }elseif (isset($_GET['categorie']) AND $_GET['categorie'] = 1){
+        # N'afficher que les articles appartenant à la catégorie 1
+      }; ?>
+
+    <?php # Liens "Page précédente" et "Page suivante" ?>
     <div class = "d-flex justify-content-between my-4">
-      <?php # La page précédente n'est visible que si la page courante est supérieure à 1
+      <?php # Page précédente
       if($offset > 1){ ?>
         <a href="articles.php?start=<?php echo $offset - 5 ?>" class="btn btn-primary">&laquo; Page précédente </a>
 
       <?php
-      }; # La page suivante n'est visible que si la page courante est inférieure au nombre de pages
+    }; # Page suivante
       if($offset + 5 < $count){ ?>
       <a href="articles.php?start=<?php echo $offset + 5 ?>" class="btn btn-primary"> Page suivante &raquo;</a>
     </div>
@@ -62,6 +78,5 @@
     // Fin de la boucle des articles
     $req->closeCursor();
     ?>
-
   </body>
 </html>
