39ddc3b42734cd9586fbe7ed02498dd64e4f1225
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-01 11:34:33 +0200

    Indentation

--- article.php
+++ article.php
@@ -7,92 +7,90 @@
       <meta charset="utf-8" />
       <title>Mon blog</title>
     	<link href="style.css" rel="stylesheet" />
-      <link rel="stylesheet" href="bootstrap.css">
-  <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Barlow&display=swap" rel="stylesheet">
-
+      <link rel="stylesheet" href="form.css">
+      <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Barlow&display=swap" rel="stylesheet">
     </head>
 
     <header>
-      <?php  include('header.php')    ?>
+      <?php  include('header.php') ?>
     </header>
 
     <body>
-        <h1>The new blog</h1>
-        <p><a href="articles.php">Retour à la liste des articles</a></p>
+      <p><a href="articles.php">Retour à la liste des articles</a></p>
+      <?php
+      // Connexion à la base de données
+      try
+      {
+      	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+      }
+      catch(Exception $e)
+      {
+        die('Erreur : '.$e->getMessage());
+      }
+      ?>
+      <div class="article">
+        <h2> Article sélectionné </h2>
+        <?php
+        # Joindre Article et Utilisateurs pour afficher le login dans "écrit par"
+        // Récupération des articles
+        if(isset($_GET['id']))
+        {
+          $req = $bdd->prepare('SELECT id_utilisateur, titre, article, date FROM articles WHERE id  = ?');
+          $req->execute(array($_GET['id']));
 
-    <?php
-    // Connexion à la base de données
-    try
-    {
-    	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-    }
-    catch(Exception $e)
-    {
-            die('Erreur : '.$e->getMessage());
-    }
+          while ($donnees = $req->fetch())
+          {
+          ?>
+          <div style="max-width: 70rem;">
+            <div class="card_articles">
+              <h2><?php echo htmlspecialchars($donnees['titre']); ?></h2>
+                écrit par <?php echo htmlspecialchars($donnees['id_utilisateur']);?>
+                le <?php echo $donnees['date']; ?><br>
+              <?php echo htmlspecialchars($donnees['article']); ?></p>
+            </div>
+          </div>
+          <?php
+          } // Fin de la boucle des articles
+          $req->closeCursor();
+        }
+        ?>
 
+        <?php
+        $req = $bdd->prepare('SELECT * FROM commentaires WHERE id_article = ?');
+        $req->execute(array($_GET['id']));
 
-    ?>
+        while ($donnees = $req->fetch())
+        {
+        ?>
+          <p> <?php echo htmlspecialchars($donnees['id_utilisateur']); ?>
+            le <?php echo $donnees['date']; ?><br>
+          <?php echo htmlspecialchars($donnees['commentaire']); ?></p>
 
+        <?php
+        } // Fin de la boucle des commentaires
+        $req->closeCursor();
 
-  <h2>Article sélectionné </h2>
+        if(isset($_POST['commentaire'])){
+          $commentaire = $_POST['commentaire'];
+          $req = $bdd->prepare("INSERT INTO commentaires(commentaire,id_article, id_utilisateur, date) VALUES(?, ?, ?, NOW())");
+          $req->execute(array($commentaire, $_GET['id'], 0)); #Mettre la variable id_utilisateur à place de 0
+          header("Refresh:0");
+        }
+        ?>
 
-  <?php
-  # Joindre Article et Utilisateurs pour afficher le login dans "écrit par"
-  // Récupération des articles
-  if(isset($_GET['id']))
-  {
-    $req = $bdd->prepare('SELECT id_utilisateur, titre, article, date FROM articles WHERE id  = ?');
-    $req->execute(array($_GET['id'])); # Ne fonctionne pas
+        <form class="form_commentaire" action="article.php?id=<?php echo $_GET['id']; ?>" method="post" name="commentaire">
+          <div class="ajout_commentaire">
+            <label> Laissez un commentaire :</label>
+            <textarea id = "commentaire" name="commentaire" value="" rows="5" cols="73"></textarea>
+          </div>
 
-    while ($donnees = $req->fetch())
-    {
-    ?>
-      <p><strong><?php echo htmlspecialchars($donnees['titre']); ?></strong>
-        écrit par <?php echo htmlspecialchars($donnees['id_utilisateur']); ?>
-        le <?php echo $donnees['date']; ?><br>
-      <?php echo htmlspecialchars($donnees['article']); ?></p>
-    <?php
-    } // Fin de la boucle des articles
-    $req->closeCursor();
-  }
-  ?>
+          <div class="submit_commentaire">
+            <input type="submit" value="Envoyer">
+          </div>
+        </form>
+      </div>
 
-  <?php
-    $req = $bdd->prepare('SELECT * FROM commentaires WHERE id_article = ?');
-    $req->execute(array($_GET['id']));
-
-    while ($donnees = $req->fetch())
-    {
-    ?>
-      <p> <?php echo htmlspecialchars($donnees['id_utilisateur']); ?>
-        le <?php echo $donnees['date']; ?><br>
-      <?php echo htmlspecialchars($donnees['commentaire']); ?></p>
-
-    <?php
-    } // Fin de la boucle des commentaires
-    $req->closeCursor();
-
-    if(isset($_POST['commentaire'])){
-      $commentaire = $_POST['commentaire'];
-      $req = $bdd->prepare("INSERT INTO commentaires(commentaire,id_article, id_utilisateur, date) VALUES(?, ?, ?, NOW())");
-      $req->execute(array($commentaire, $_GET['id'], 0)); #Mettre la variable id_utilisateur à place de 0
-      header("Refresh:0");
-    }
-    ?>
-
-  <form class="form_commentaire" action="article.php?id=<?php echo $_GET['id']; ?>" method="post" name="commentaire">
-    <div class="ajout_commentaire">
-      <label> Laissez un commentaire :</label>
-      <textarea id = "commentaire" name="commentaire" value="" rows="5" cols="33"></textarea>
-    </div>
-
-    <div class="submit_commentaire">
-      <input type="submit" value="Envoyer">
-    </div>
-  </form>
-  <?php include("footer.php");
-   ?>
-  </body>
+      <?php include("footer.php");?>
+    </body>
 </html>
