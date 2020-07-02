5fc548ac2fcf568a99ea889f91f669a18e64ce95
cecile-wojnowski <62134503+cecile-wojnowski@users.noreply.github.com>
2020-07-02 19:21:22 +0200

    Requête et condition empêchant l'existence en double d'un login

--- inscription.php
+++ inscription.php
@@ -1,24 +1,29 @@
 <?php
+$mysqli = mysqli_connect("127.0.0.1", "root", "", "blog"); # Connexion à la base de données
+$mysqli->set_charset("utf8"); # Permet d'afficher les accents
+
 # Si le champ login est rempli
 if(isset($_POST["login"])) {
+  $sql = "SELECT * FROM utilisateurs WHERE login = '".$_POST['login']."'";
+  $requete=$mysqli->query($sql);
+
   if($_POST["password"] != $_POST["confirm_password"]) {
     /* Si le mot de passe confirmé n'est pas identique au premier mot de passe saisi :
     Rediriger vers inscription avec un message d'erreur */
     header('Location: inscription.php?erreur=1');
-  } else {
-    $mysqli = mysqli_connect("127.0.0.1", "root", "", "blog"); # Connexion à la base de données
-    $mysqli->set_charset("utf8"); # Permet d'afficher les accents
 
-    # Est enregistré dans des variables ce que l'utilisateur entre dans les champs du formulaire
+  } elseif($requete->num_rows > 0) { # Si le login provenant du form existe déjà dans la bdd, on redirige
+    header('Location: inscription.php?erreur=2');
+  } else {
     $login = $_POST['login'] ;
     $password = $_POST['password'];
     $email= $_POST['email'];
     $id_droits = $_POST['id_droits'];
     $passcrypt = password_hash($password, PASSWORD_BCRYPT);
 
-# Requête : insérer dans la table Utilisateurs les données correspondant à ces variables
-$sql = "INSERT INTO utilisateurs(login, password, email, id_droits)
-        VALUES('$login', '$passcrypt', '$email', '$id_droits')";
+    # Requête : insérer dans la table Utilisateurs les données correspondant à ces variables
+    $sql = "INSERT INTO utilisateurs(login, password, email, id_droits)
+            VALUES('$login', '$passcrypt', '$email', '$id_droits')";
 
     # Exécution de la requête :
     if ($mysqli->query($sql) === TRUE) {
@@ -53,7 +58,12 @@ $sql = "INSERT INTO utilisateurs(login, password, email, id_droits)
         <form action="inscription.php" method="POST" name="inscription">
           <?php
           if (isset($_GET['erreur'])){
-            echo "Veuillez confirmer votre mot de passe.";
+            if ($_GET["erreur"] == 1) {
+              echo "Veuillez confirmer votre mot de passe.";
+            } elseif($_GET["erreur"] == 2) {
+              echo "Ce login existe déjà.";
+            }
+
           }
           ?>
     	    <div>
